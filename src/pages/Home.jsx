271778caf5b43c898e6840bb0cb1a3636d71a680
lex watts <lunalovesless77@gmail.com>
2026-07-17 20:34:04 -0600

    imported work from previous repo, Tailwind CSS is set up as well as theme toggle

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "../components/ThemeToggle.jsx";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        
+    {/* Theme */}
+        <ThemeToggle />
+    {/* Background FX */}
+
+    {/* Navbar */}
+
+    {/* Main Content */}
+
+
+    {/* Footer */}
+    </div>
+  );
+};
